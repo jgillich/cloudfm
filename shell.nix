d4180886013a727bf7dd64cf16b7f3d3568ead0a
Jakob Gillich <jakob@gillich.me>
2016-03-21 18:03:13 -0400

    nix: enable couchdb

--- shell.nix
+++ shell.nix
@@ -1,29 +1,63 @@
-{
+with import <nixpkgs> { };
+
+rec {
+  configFile = writeText "couchdb.ini" ''
+    [couchdb]
+    database_dir = /tmp/cloudfm/data
+    uri_file = /tmp/cloudfm/couchdb.uri
+    view_index_dir = /tmp/cloudfm/data
+    [httpd]
+    port = 15984
+    bind_address = 127.0.0.1
+    [log]
+    file = /tmp/cloudfm/couchdb.log
+  '';
+
   shellHook = ''
     set -e
 
-    APP=`pwd`/app
-    SERVER=`pwd`/server
+    function dirs {
+      APP=`pwd`
+      while [[ ! -f "$APP/LICENSE" ]]; do APP+="/.."; done
+      APP+="/app"
+
+      SERVER=`pwd`
+      while [[ ! -f "$SERVER/LICENSE" ]]; do SERVER+="/.."; done
+      SERVER+="/server"
+    }
 
     function migrate {
-      pushd $SERVER; diesel migration run; popd
+      dirs; pushd $SERVER; diesel migration run; popd
     }
 
     function build {
+      dirs
       pushd $APP; elm-package install -y && elm-make --yes src/App.elm; popd
       pushd $SERVER; cargo build; popd
     }
 
     function test {
+      dirs
       pushd $SERVER; cargo test; popd
     }
 
     function ci {
+      couchdb &
       build; test
     }
 
     function run {
+      dirs
+      couchdb &
       pushd $SERVER; cargo run; popd
+    }
+
+    function couchdb {
+      export DATABASE_URL="http://localhost:15984"
+      rm -rf /tmp/cloudfm
+      mkdir -p /tmp/cloudfm/data
+      touch /tmp/cloudfm/couchdb.ini
+      ${couchdb}/bin/couchdb -a ${configFile} -a /tmp/cloudfm/couchdb.ini
     }
   '';
 }
