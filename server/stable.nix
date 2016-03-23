acfa054d5b1e71f4614573504e9ec691ce2a464a
Jakob Gillich <jakob@gillich.me>
2016-03-23 15:31:53 +0100

    server: update deps hashes

--- server/stable.nix
+++ server/stable.nix
@@ -1,10 +1,9 @@
 with import <nixpkgs> { };
-
-rustPlatform.buildRustPackage rec {
-  name = "cloudfm-server-${version}";
-  version = "0.1";
-  src = ./.;
+let
+  self = import ./default.nix;
+in rustPlatform.buildRustPackage {
+  inherit (self) name src shellHook;
   buildInputs = [ openssl postgresql sqlite pkgconfig ];
-  depsSha256 = "160ar8jfzhhrg5rk3rjq3sc5mmrakysynrpr4nfgqkbq952il2zk";
-  shellHook = (import ../shell.nix).shellHook;
+  buildPhase = "cargo build --release --features stable --no-default-features";
+  depsSha256 = "1qfpdbj45cqy1ppwnzp763qgks5afw9yy467z4smml6x1sd78pip";
 }
