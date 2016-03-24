00bc0c49c99697cbef5b1fce7e3295bb446393da
Jakob Gillich <jakob@gillich.me>
2016-03-24 13:38:22 +0100

    server: refactor init code into constructor

--- server/src/server.rs
+++ server/src/server.rs
@@ -7,7 +7,7 @@ use iron::Iron;
 use super::backends::{Dropbox, Fs, Spotify, Backend};
 
 pub struct Server {
-    mount: Mount,
+    handler: Mount,
     db: chill::Client,
     backends: Vec<Box<Backend>>
 }
@@ -16,49 +16,39 @@ impl Server {
 
     pub fn new() -> Server {
         let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        let db = chill::Client::new(&db_url).expect("DATABASE_URL must be a valid URL");
 
+        // create backends
         let backends: Vec<Box<Backend>> = vec![
             Box::new(Dropbox::new()),
             Box::new(Fs::new()),
             Box::new(Spotify::new()),
         ];
 
+        // create databases
+        for path in vec![ "/tracks" ] {
+            if let Err(e) = db.create_database("/tracks").run() {
+                match e {
+                    chill::Error::DatabaseExists(err) => (),
+                    _ => Err(Error::from(e)).unwrap()
+                }
+            }
+        }
+
+        // create routes
+        let mut mount = Mount::new();
+        let router = Router::new();
+        mount.mount("/v1", router);
+
         Server {
-            mount: Mount::new(),
-            db: chill::Client::new(&db_url).expect("DATABASE_URL must be a valid URL"),
+            handler: mount,
+            db: db,
             backends: backends,
         }
     }
 
     pub fn start(mut self) {
-        self.init_db().expect("failed to initialize database");
-        self.init_routes().expect("failed to initialize routes");
-
-        Iron::new(self.mount).http("localhost:3000").unwrap();
-    }
-
-    fn init_db(&mut self) -> Result<(), Error> {
-        let paths = vec![ "/tracks" ];
-
-        for path in paths {
-            if let Err(e) = self.db.create_database("/tracks").run() {
-                match e {
-                    chill::Error::DatabaseExists(err) => (),
-                    _ => return Err(Error::from(e))
-                }
-            }
-        }
-
-        Ok(())
-    }
-
-    fn init_routes(&mut self) -> Result<(), Error> {
-        let router = Router::new();
-
-        //router.get("/tracks/:uri", )
-
-        self.mount.mount("/v1", router);
-        Ok(())
+        Iron::new(self.handler).http("localhost:3000").unwrap();
     }
 
     fn index(&mut self) -> Result<(), Error> {
