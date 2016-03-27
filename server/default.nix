3958fd09e4a5eb12537cf374ce9ab25de864fef7
Jakob Gillich <jakob@gillich.me>
2016-03-27 16:36:12 +0200

    chore(nix): update deps hash

--- server/default.nix
+++ server/default.nix
@@ -5,6 +5,6 @@ rustUnstable.buildRustPackage rec {
   version = "0.1";
   src = ./.;
   buildInputs = [ openssl postgresql sqlite pkgconfig ];
-  depsSha256 = "05z9d13rbym7nmmc11rmy5gmxs8hjw659g1j7pgns7rz9m6prkkz";
+  depsSha256 = "0g1fzs1b7rrbs07cnm6qi6jrzqm0kmnxb2ykjf62zawr0392l4g9";
   shellHook = (import ../shell.nix).shellHook;
 }
