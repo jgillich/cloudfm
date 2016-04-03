90f148633655d29d48907896a91fb1f0fba8da6f
Béla Wäckerlig <bwackerlig@localhost.localdomain>
2016-04-03 12:53:52 +0200

    add redux-logger middleware; fix tsconfig.json to make atom-typescript work smoothly

--- app/src/store/configureStore.ts
+++ app/src/store/configureStore.ts
@@ -2,11 +2,14 @@ import { createStore, applyMiddleware, compose } from "redux"
 import { persistentStore } from 'redux-pouchdb'
 import rootReducer from "../reducers"
 import * as PouchDB from "pouchdb";
+import * as createLogger from "redux-logger"
 
 const db = new PouchDB("cloudfm");
 
 const applyMiddlewares = applyMiddleware(
-
+  
+  // HAS TO BE LAST MIDDLEWARE
+  (createLogger as any)()
 );
 
 const createStoreWithMiddleware = compose(
