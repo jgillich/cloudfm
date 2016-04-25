df0c2cdfa3e28f705a13511143a6285c7e5abb1b
Jakob Gillich <jakob@gillich.me>
2016-04-25 22:43:16 +0200

    fix(app): fake Action string enum

--- app/src/actions/index.ts
+++ app/src/actions/index.ts
@@ -3,22 +3,31 @@ export * from "./player";
 export * from "./user";
 export * from "./artist";
 
-export enum Action {
-  InsertArtist,
-  RemoveArtist,
-  UpdateArtist,
+// Workaround until we get proper string enums
+// https://github.com/Microsoft/TypeScript/issues/3192
+export type Action = "INSERT_ARTIST" | "UPDATE_ARTIST" | "REMOVE_ARTIST" |
+  "INSERT_TRACK" | "UPDATE_TRACK" | "REMOVE_TRACK" |
+  "PLAY_ALBUM" | "PLAY_ARTIST" | "PLAY_TRACK" |
+  "LOGIN_USER" | "SIGNUP_USER" | "UPDATE_USER" |
+  "RESET_ERROR";
 
-  InsertTrack,
-  RemoveTrack,
-  UpdateTrack,
+/* tslint:disable:object-literal-sort-keys */
+export const Action = {
+  InsertArtist: "INSERT_ARTIST" as Action,
+  RemoveArtist: "UPDATE_ARTIST" as Action,
+  UpdateArtist: "REMOVE_ARTIST" as Action,
 
-  PlayAlbum,
-  PlayArtist,
-  PlayTrack,
+  InsertTrack: "INSERT_TRACK" as Action,
+  RemoveTrack: "UPDATE_TRACK" as Action,
+  UpdateTrack: "REMOVE_TRACK" as Action,
 
-  LoginUser,
-  SignupUser,
-  UpdateUser,
+  PlayAlbum: "PLAY_ALBUM" as Action,
+  PlayArtist: "PLAY_ARTIST" as Action,
+  PlayTrack: "PLAY_TRACK" as Action,
 
-  ResetError,
+  LoginUser: "LOGIN_USER" as Action,
+  SignupUser: "SIGNUP_USER" as Action,
+  UpdateUser: "UPDATE_USER" as Action,
+
+  ResetError: "RESET_ERROR" as Action,
 };
