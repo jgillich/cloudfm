60263c941fe40c42058ba4d0472011ec40e1bbf5
Jakob Gillich <jakob@gillich.me>
2016-05-07 21:01:22 +0000

    chore(nix): remove nix config

--- nix/docker/couchdb.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-with pkgs;
-dockerTools.pullImage {
-  imageName = "couchdb";
-  imageTag = "1.6-couchperuser";
-  sha256 = "06qlxjg5i66h72dspcrf2blypsyr3kw2pdgx9v1n0bbrdz03iqmm";
-}
